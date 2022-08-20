084656a0391e15d199eb08f3947a33f479bc0cc6
satyarrr <satya9c27@gmail.com>
2022-08-20 14:05:28 +0800

    changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,8 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="footer">
-      <a >
-        React Notes App
+      <a href="/">
+        Notes Taking APP
       </a>
     </footer>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
-import Search from "./Search";
-import { useState } from "react";
-import Note from "./Note";
+// import Search from "./Search";
+// import { useState } from "react";
+// import Note from "./Note";
 
 const Header = () => {
-  const [searchText, setSearchText] = useState('');
+  // const [searchText, setSearchText] = useState('');
   return (
     <Navbar bg="dark" variant="dark">
       <Container>
